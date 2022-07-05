017db6d721794e5294a66987485bd7a43729e190
happy902 <107293124+happy902@users.noreply.github.com>
2022-07-04 23:51:56 -0500

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,19 @@
 export default {
     async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
+        let url = new URL(request.url);
+        if (url.pathname.startsWith('/')) {
+            
+            let nd = new Date();
+            if (nd.getDate() % 2) {
+                url.hostname="app1.herokuapp.com";
+            } else {
+                url.hostname="app2.herokuapp.com";
+            }
+
+            let new_request=new Request(url,request);
+            return fetch(new_request);
+        }
+        // Otherwise, serve the static assets.
+        return env.ASSETS.fetch(request);
     }
-  };
+};
